5abf62f56090ed57140c485260ae9828aa6e6244
aaryan200 <co21btech11001@iith.ac.in>
2023-04-17 21:43:55 +0530

    commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const cooky = document.querySelector("body > div.ff-sans.ps-fixed.z-nav-fixed.ws4.sm\\:w-auto.p32.sm\\:p16.bg-black-750.fc-white.bar-lg.b16.l16.r16.js-consent-banner");
+
+if (cooky) {
+    cooky.remove();
+}
